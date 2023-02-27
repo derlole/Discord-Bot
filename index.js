e1bc2089de2587e72876d85122543d192442d1a6
derlole <122916573+derlole@users.noreply.github.com>
2023-02-27 19:48:05 +0000

    i

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+const path = require('path')
 const { Client } = require('discord.js')
+const wokcommands = require('wokcommands')
+const keepAlive = require('./server.js')
+
 const config = require('./config.json')
+
 const client = new Client({intents: 98303})
+
 client.on('ready', () => {
     client.user.setActivity("SUBNAUTICA", {type: "PLAYING"})
-    console.log("WWWWWOOOOOOOOOWWWWWW")
+
+    new wokcommands(client, {
+        youtubeDL: false,
+        showWarns: false,
+        commandsDir: path.join(dirname, 'commands'),
+        featuresDir: path.join(dirname, 'features'),
+        testServers: ['1079807641896890488'],
+        disabledDefaultCommands: [
+            'command',
+            'language',
+            'prefix',
+            'requiredrole',
+            'channelonly'
+        ],
+        botOwners: [config.ownerID],
+    })
+    .setDefaultPrefix(config.Prefix)
 })
-client.login(config.token)
+
+client.login(process.env['Token'])
+keepAlive()

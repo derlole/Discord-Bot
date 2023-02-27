ec30b00f1324232ac2a68d9010ba6ee136ac919f
derlole <122916573+derlole@users.noreply.github.com>
2023-02-27 20:33:20 +0000

    hi

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 const path = require('path')
-const { Client } = require('discord.js')
+const { Client, IntentsBitField, Partials } = require("discord.js")
 const wokcommands = require('wokcommands')
 const keepAlive = require('./server.js')
 
 const config = require('./config.json')
 
-const client = new Client({intents: 98303})
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.DirectMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+  partials: [Partials.Channel],
+})
 
 client.on('ready', () => {
-    client.user.setActivity("SUBNAUTICA", {type: "PLAYING"})
+  client.user.setActivity("SUBNAUTICA", { type: "PLAYING" })
 
-    new wokcommands(client, {
-        youtubeDL: false,
-        showWarns: false,
-        commandsDir: path.join(dirname, 'commands'),
-        featuresDir: path.join(dirname, 'features'),
-        testServers: ['1079807641896890488'],
-        disabledDefaultCommands: [
-            'command',
-            'language',
-            'prefix',
-            'requiredrole',
-            'channelonly'
-        ],
-        botOwners: [config.ownerID],
-    })
-    .setDefaultPrefix(config.Prefix)
+  new wokcommands({
+    client,
+    youtubeDL: false,
+    showWarns: false,
+    commandsDir: path.join(__dirname, 'commands'),
+    //featuresDir: path.join(__dirname, 'features'),
+    testServers: ['1079807641896890488'],
+    disabledDefaultCommands: [
+      'command',
+      'language',
+      'prefix',
+      'requiredrole',
+      'channelonly'
+    ],
+    botOwners: [config.ownerID],
+  })
+
 })
 
 client.login(process.env['Token'])

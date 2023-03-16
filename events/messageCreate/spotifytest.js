87f81d2a0c9a32341e05072167f4750d5b4a6c87
derlole <122916573+derlole@users.noreply.github.com>
2023-03-16 17:43:29 +0000

    songdingens

--- events/messageCreate/spotifytest.js
+++ events/messageCreate/spotifytest.js
@@ -5,29 +5,75 @@ module.exports = (message, instance) => {
   
   
   if (message.content === "Zeit" && message.channel.id ==='1079807642639290510') {
-    let index = 0;
+    let songnubmer = 0;
    const intervalId = setInterval(() => {
-      if (index < 3) {
+      if (songnubmer < 1) {
       const embed = new EmbedBuilder()
-        .setTitle(`Song ${index}`)
+        .setTitle(`Song ${songnubmer}`)
         .addFields(
           {
-            name: "Time",
-            value: spotify[index].ms_played /60000 +"min"
+            name: "Streaming Zeitpunkt",
+            value: spotify[songnubmer].ts,
+            //inline: true
           },
           {
-            name: "Song",
-            value: spotify[index].master_metadata_track_name,
-            inline: true
+            name: "Nutzer",
+            value: spotify[songnubmer].username,
+            //inline: true
+          },
+          {
+            name: "Gestreamet über",
+            value: spotify[songnubmer].platform,
+            //inline: true
+          },
+          {
+            name: "Time Played",
+            value: spotify[songnubmer].ms_played /60000 +"min", //aus millisekunden Zeit in Minuten und Sekunden umrechnen
+            //inline:true
           },
           {
             name: "Genutzete IP",
-            value: spotify[index].ip_addr_decrypted,
-            inline: true
-          }
+            value: spotify[songnubmer].ip_addr_decrypted,
+            //inline: true
+          },
+          {
+            name: "Song",
+            value: spotify[songnubmer].master_metadata_track_name,
+            //inline: true
+          },
+          {
+            name: "Künstler",
+            value: spotify[songnubmer].master_metadata_album_artist_name,
+            //inline: true
+          },
+          {
+            name: "Album",
+            value: spotify[songnubmer].master_metadata_album_album_name,
+            //inline: true
+          },
+          {
+            name: "Startgrund",
+            value: spotify[songnubmer].reason_start,
+            //inline: true 
+          },
+          {
+            name: "Endgrund",
+            value: spotify[songnubmer].reason_end,
+            //inline: true
+          },
+          {
+            name: "Shuffle",
+            value: spotify[songnubmer].shuffle.toString(),
+            //inline: true
+          },
+          {
+            name: "Offline-Stream",
+            value: spotify[songnubmer].offline.toString(),
+            //inline: true
+          },
         )
       message.channel.send({embeds: [embed]});
-      index++;
+      songnubmer++;
       } else {
         clearInterval(intervalId);
       }

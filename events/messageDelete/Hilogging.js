94aac4bd029db586fd42e744d59f60e8a70c753d
derlole <122916573+derlole@users.noreply.github.com>
2023-03-17 15:09:37 +0000

    Pommes

---
+++ events/messageDelete/Hilogging.js
@@ -0,0 +1,25 @@
+const { EmbedBuilder, Client } = require('discord.js');
+
+module.exports =  async (message, client, instance) => {
+  if (message.guild.id === "849905679271329803") {
+    const logChannel = await message.client.channels.fetch('1086019415927169085');
+    const embed = new EmbedBuilder()
+    .setTitle(Deleted Message)
+    .setColor('#ff00ff')
+    .addFields(
+        {
+            name: "Channel",
+            value: message.channel.name,
+        },
+        { 
+            name: "User",
+            value: message.author.tag,
+        },
+        {
+            name: "Message",
+            value: message.content,
+        }
+    )
+   await logChannel.send({embeds: [embed]});
+  }
+}

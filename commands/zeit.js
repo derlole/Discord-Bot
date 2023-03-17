0e97bec17b10ae82d8cbfe0bf0121ac228feda3f
derlole <122916573+derlole@users.noreply.github.com>
2023-03-17 22:55:31 +0000

    Zeitzeugs

---
+++ commands/zeit.js
@@ -0,0 +1,54 @@
+const { CommandType } = require("wokcommands")
+const { EmbedBuilder } = require('discord.js');
+const spotifyEins = require('.././songdata0.json')
+const spotifyZwei = require('.././songdata1.json')
+const spotifyDrei = require('.././songdata2.json')
+const spotifyVier = require('.././songdata3.json');
+const song = require("./song");
+const spotify = spotifyEins.concat(spotifyZwei, spotifyDrei, spotifyVier)
+console.log(spotify.length)
+/*function formatMilliseconds(milliseconds) {
+  if (!milliseconds) return 'no wert bruder'
+  let seconds = Math.floor(milliseconds / 1000) % 60
+  let minutes = Math.floor(milliseconds / 60000)
+  return `${minutes.toString()}:${seconds.toString().padStart(2, '0')}`
+}*/
+
+module.exports = {
+  description: 'Give Total Play time up to an specific song',
+  type: CommandType.LEGACY,
+  minArgs: 1,
+  maxArgs: 1,
+  expectedArgs: "<songarray-number>",
+  ownerOnly: true,
+    callback: ({ args, message, channel }) => {
+    
+    var songnumber = 0;
+    const track = args[0];
+    var time = 0;
+    //if (!songnumber) return 'Please provide a song number';
+    if (isNaN(songnumber)) return 'Please to be not an idiot';
+    if (songnumber < 0) return 'Please provide to be not an idiot';
+    if (songnumber > spotify.length - 1) return 'Please provide a valid number for the songs';
+    const intervalId = setInterval(() => {
+        if (track > songnumber) {
+            time = time + spotify[songnumber].ms_played
+            songnumber++
+            console.log(time)
+        } else {
+        clearInterval(intervalId);
+        
+    }
+    },10);
+    const embed = new EmbedBuilder()
+        .setTitle(`Playtime`)
+        .addFields(
+            {
+                name: "Total Playtime",
+                value: "Zeit: " + time + " ms",
+            }
+        )
+        .setColor('#ff00ff')
+        message.channel.send({embeds: [embed]});
+    }
+}
